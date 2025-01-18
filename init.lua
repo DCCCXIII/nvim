a1ec5c7a1e2dbc1432df82d49566e76a9da90530
Ruben Ruiz Marron <rubenruizmarron@gmail.com>
2025-01-18 11:57:49 -0600

    installed treesitter

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 vim.g.mapleader= " "
 vim.cmd("set relativenumber")
 vim.cmd("set colorcolumn=80")
+vim.cmd("set scrolloff=12")
 
 -- Check for Lazy, install if not present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -22,14 +23,26 @@ local plugins = {
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000},
 	{ 'nvim-telescope/telescope.nvim', tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim'}
-	}
+	},
+	{"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}
 }
 
 require("lazy").setup(plugins, opts)
-require("catppuccin").setup()
 
+-- catppuccin
+require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
+-- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+	-- see supported languages in https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+	ensure_installed = {"lua", "javascript", "go", "rust"},
+	highlight = { enable = true },
+	indent = { enable = true }
+})
