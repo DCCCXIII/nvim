a04ddb7574148b193009b8fa8cfae6fe4882ee2f
Ruben Ruiz Marron <rubenruizmarron@gmail.com>
2025-01-28 20:30:58 -0600

    changed theme to Kanagawa Wave

--- init.lua
+++ init.lua
@@ -38,15 +38,15 @@ local plugins = {
 	{"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
 	{"mbbill/undotree"},
 	{
-	  'projekt0n/github-nvim-theme',
-	  name = 'github-theme',
+	  'rebelot/kanagawa.nvim',
+	  name = 'kanagawa',
 	  lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	  priority = 1000, -- make sure to load this before all the other start plugins
 	  config = function()
-	    require('github-theme').setup({
+	    require('kanagawa').setup({
 	    })
 
-	    vim.cmd('colorscheme github_dark_high_contrast')
+	    vim.cmd('colorscheme kanagawa-wave')
 	  end,
 	},
 	{'neovim/nvim-lspconfig'},
