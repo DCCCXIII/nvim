c87950db87c5cded123487e19942c0015870f886
Ruben RM <rubenruizmarron@gmail.com>
2026-02-13 19:37:47 -0600

    refactor

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.6',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
+    { '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Git files' },
+    { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Live grep' },
+  },
+}
