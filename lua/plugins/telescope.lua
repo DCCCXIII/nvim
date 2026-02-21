d8f28e9fe9322eb92c7edf26c9cd208b8ae6a9c6
Ruben RM <rubenruizmarron@gmail.com>
2026-02-20 22:48:11 -0600

    added more telescope keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,9 @@ return {
   tag = '0.1.6',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
+    { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
     { '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Git files' },
     { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Live grep' },
+    { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'Buffers' },
   },
 }
