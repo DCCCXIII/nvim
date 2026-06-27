21c6c2e7b432b4627e88b98e21b2c057bd2d4afe
Ruben Ruiz <rubenruizmarron@gmail.com>
2026-06-27 10:46:32 -0600

    feat: added display for spaces, etc. added keybinds

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,4 +7,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.listchars = { tab = ">--", space = "•", trail = "~"}
+-- LSP
 vim.opt.signcolumn = 'yes'
